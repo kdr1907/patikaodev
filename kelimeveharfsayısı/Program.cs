6a786639bae0a472bc125ce67cc0a225a262886d
Kadir <kdrokszs7@gmail.com>
2025-03-04 19:04:49 +0300

    .

---
+++ kelimeveharfsayısı/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lütfen bir cümle girin: ");
+        string cumle = Console.ReadLine();
+
+        string[] kelimeler = cumle.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        int kelimeSayisi = kelimeler.Length;
+
+        int harfSayisi = 0;
+        foreach (string kelime in kelimeler)
+        {
+            harfSayisi += kelime.Length;
+        }
+
+        Console.WriteLine($"Cümledeki kelime sayısı: {kelimeSayisi}");
+        Console.WriteLine($"Cümledeki harf sayısı: {harfSayisi}");
+    }
+}
---
+++ kelimeyazdir/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lütfen pozitif bir sayı (n) girin: ");
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen pozitif bir sayı girin.");
+            Console.Write("Lütfen pozitif bir sayı (n) girin: ");
+        }
+
+        List<string> kelimeler = new List<string>();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Lütfen {i + 1}. kelimeyi girin: ");
+            string kelime = Console.ReadLine();
+            kelimeler.Add(kelime);
+        }
+
+        Console.WriteLine("Girilen kelimeler sondan başa doğru:");
+        for (int i = kelimeler.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(kelimeler[i]);
+        }
+    }
+}
