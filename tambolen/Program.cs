a1cdca88bba803067b08b1e828d29a0967e768bb
Kadir <kdrokszs7@gmail.com>
2025-03-04 18:31:25 +0300

    d

---
+++ tambolen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Lütfen pozitif bir sayı (n) girin: ");
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= 0)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen pozitif bir sayı girin.");
+            Console.Write("Lütfen pozitif bir sayı (n) girin: ");
+        }
+
+        Console.Write("Lütfen pozitif bir sayı (m) girin: ");
+        int m;
+        while (!int.TryParse(Console.ReadLine(), out m) || m <= 0)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen pozitif bir sayı girin.");
+            Console.Write("Lütfen pozitif bir sayı (m) girin: ");
+        }
+
+        List<int> sayilar = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Lütfen {i + 1}. pozitif sayıyı girin: ");
+            int sayi;
+            while (!int.TryParse(Console.ReadLine(), out sayi) || sayi <= 0)
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen pozitif bir sayı girin.");
+                Console.Write($"Lütfen {i + 1}. pozitif sayıyı girin: ");
+            }
+            sayilar.Add(sayi);
+        }
+
+        Console.WriteLine($"Girilen sayılardan {m}'e eşit veya tam bölünenler:");
+        foreach (int sayi in sayilar)
+        {
+            if (sayi == m || sayi % m == 0)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
--- tambolen/obj/Debug/net9.0/tambolen.AssemblyInfo.cs
+++ tambolen/obj/Debug/net9.0/tambolen.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("girilenciftsayılarıyazdır")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("tambolen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5bcb5ee9ce3bcf2cec23442fca2963a319fa6b3")]
-[assembly: System.Reflection.AssemblyProductAttribute("girilenciftsayılarıyazdır")]
-[assembly: System.Reflection.AssemblyTitleAttribute("girilenciftsayılarıyazdır")]
+[assembly: System.Reflection.AssemblyProductAttribute("tambolen")]
+[assembly: System.Reflection.AssemblyTitleAttribute("tambolen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
